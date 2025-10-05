b772755f1f981a73455d269a9f9033ccef2ef72d
Divyansh Arora <141400127+Aroraboy@users.noreply.github.com>
2025-10-06 02:31:43 +0530

    connection to mongo atlas

--- src/index.js
+++ src/index.js
@@ -1,23 +1,41 @@
-import mongoose from "mongoose"
-import {DB_NAME} from './constants.js'
+// require('dotenv').config({path: './env'})
+import dotenv from 'dotenv'
 
-import express from "express"
-const app = express()
+i
+import connectDB from "./db/index.js"
 
-( async () => {
-    try {
-        await mongoose.connect('${process.env.MONGODB_URI}/${DB_NAME}')
-        app.on("error", (error) => {
-            console.log("ERRR: ", error)
-            throw error
-        })
+dotenc.config({ path: './.env' })
 
-        app.listen(process.env.PORT, () => {
-            console.log('App is listening on port $ {process.env.PORT}');
-        })
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+// import express from "express"
+
+// const app = express()
+
+// ( async () => {
+//     try {
+//         await mongoose.connect('${process.env.MONGODB_URI}/${DB_NAME}')
+//         app.on("error", (error) => {
+//             console.log("ERRR: ", error)
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log('App is listening on port $ {process.env.PORT}');
+//         })
         
-    } catch (error) {
-        console.error("ERROR: ", error)
-        throw err
-    }
-})()
+//     } catch (error) {
+//         console.error("ERROR: ", error)
+//         throw err
+//     }
+// })()
